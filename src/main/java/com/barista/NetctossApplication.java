acee2d9809f52d4bd393d2b7fe9122400d529a4f
597611084@qq.com <597611084@qq.com>
2019-09-05 21:28:02 +0800

    可视化界面查看项目方法执行流程的框架
    <!--/tree/project.html         返回完整调用链视图-->
    <!--/tree/project.html?all=1 返回全部方法视图-->
    <!--/projecttree/all         JSON形式的返回结果-->
    <!--/projecttree/{methodId}     对某一方法的JSON结果-->

--- src/main/java/com/barista/NetctossApplication.java
+++ src/main/java/com/barista/NetctossApplication.java
@@ -1,6 +1,5 @@
 package com.barista;
 
-import com.barista.util.FileUtil;
 import com.barista.util.RedisUtil;
 
 import org.springframework.boot.SpringApplication;
@@ -10,16 +9,19 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import cn.yueshutong.springprojecttree.config.annotation.EnableProjectTree;
+
+
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 @EnableCaching
 @EnableTransactionManagement
+@EnableProjectTree(value = "execution(* com.barista..*.*(..))")
 public class NetctossApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NetctossApplication.class, args);
-        new RedisUtil();
     }
 
 
